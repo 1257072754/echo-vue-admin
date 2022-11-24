8adebdba5c70a76746969d3b4fba87b2ca75f540
Hakase <1257072754@qq.com>
2022-11-24 23:42:18 +0800

    Initial commit

---
+++ .cz-config.js
@@ -0,0 +1,65 @@
+'use strict'
+module.exports = {
+  types: [
+    { value: '✨新增', name: '新增:    新的内容' },
+    { value: '🐛修复', name: '修复:    修复一个Bug' },
+    { value: '📝文档', name: '文档:    变更的只有文档' },
+    { value: '💄格式', name: '格式:    空格, 分号等格式修复' },
+    { value: '♻️重构', name: '重构:    代码重构，注意和特性、修复区分开' },
+    { value: '⚡️性能', name: '性能:    提升性能' },
+    { value: '✅测试', name: '测试:    添加一个测试' },
+    { value: '🔧工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
+    { value: '⏪回滚', name: '回滚:    代码回退' },
+  ],
+  // scope 类型（定义之后，可通过上下键选择）
+  scopes: [
+    ['components', '组件相关'],
+    ['hooks', 'hook 相关'],
+    ['utils', 'utils 相关'],
+    ['element-ui', '对 element-ui 的调整'],
+    ['styles', '样式相关'],
+    ['deps', '项目依赖'],
+    ['auth', '对 auth 修改'],
+    ['other', '其他修改'],
+    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+    ['custom', '以上都不是？我要自定义'],
+  ].map(([value, description]) => {
+    return {
+      value,
+      name: `${value.padEnd(30)} (${description})`,
+    }
+  }),
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },  */
+  // 交互提示信息
+  messages: {
+    type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
+    scope: '\n选择一个 scope（可选）：',
+    // 选择 scope: custom 时会出下面的提示
+    customScope: '请输入自定义的 scope：',
+    subject: '填写简短精炼的变更描述：\n',
+    body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
+    breaking: '列举非兼容性重大的变更（可选）：\n',
+    footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
+    confirmCommit: '确认提交？',
+  },
+
+  // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
+  allowBreakingChanges: ['feat', 'fix'],
+
+  // 跳过要询问的步骤
+  // skipQuestions: ['body', 'footer'],
+
+  // subject 限制长度
+  subjectLimit: 100,
+  breaklineChar: '|', // 支持 body 和 footer
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true,
+}
